0947a1e910b9e8e73a519f8311fa530add1edb98
Dragonmage337 <82111850+Dragonmage337@users.noreply.github.com>
2022-03-16 13:25:45 -0700

    Update "MAIN".py

--- "MAIN".py
+++ "MAIN".py
@@ -10,7 +10,7 @@ def StartGame():
     x = input("Hello there, Users! Would you like to play a game?")
     if x == 'y' or x == 'yes':
         Main()
-    elif x.lower == 'n' or x.lower == 'no':
+    elif x == 'n' or x == 'no':
         print("Ok, I guess.")
         time.sleep(10)
         StartGame()
@@ -18,11 +18,20 @@ def StartGame():
 def Main():
     print("Player 1, roll the 'dice'")
     xa = input("Roll?")
-    if xa.lower == 'y' or xa.lower == "yes":
+    if xa == 'y' or xa == "yes":
         p1 = random.randint(1,100)
         print("Ok, next it is Player 2's turn")
     xb = input("Roll?")
-    if xb.lower == 'y' or xb.lower == "yes":
+    if xb == 'y' or xb == "yes":
         p2 = random.randint(1,100)
-        print("Alright Both players have rolled!")
+        print("Alright both players have rolled!")
+
+
+def Decision():
+    print("Now to get to the interesting part!")
+    time.sleep(1)
+    print("BETTING HAHAHHAHAHHAHAH!!!!!")
+    ya
+
+    
 StartGame()
