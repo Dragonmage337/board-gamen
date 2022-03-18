6476fcceedfd4058f07d90be6793fb5062bcad76
Dragonmage337 <82111850+Dragonmage337@users.noreply.github.com>
2022-03-18 08:29:58 -0700

    he he he
    haw

---
+++ RefScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+//This is a comment. The computer ignores this but keeps it in the code anyway.
+
+    // Update is called once per frame
+    //This is a function
+    void Update()
+    {
+        //This is a variable
+       int i = 0;
+       //This is a loop
+       while (i < 5)
+       {
+           //This is a statement
+           Debug.Log("Printed");
+       }
+    }
+}
---
+++ Reference Script.py
@@ -0,0 +1,23 @@
+import imp
+from traceback import print_tb
+
+
+print("This is a statement")
+#This is a function
+def Function():
+    #This is a loop.
+    while True:
+        #This is a statement.
+        print("Something.")
+#This is a variable.
+var = 1
+#This is a comment
+#Useful stuff
+#Tells the computer to wait times idk
+import time
+#Choose a random number, word, or something.
+import random
+
+DICTIONARY = {
+    "This is a dictionary" : True
+}
